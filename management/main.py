e35f145bc01cf3afc73578bb7aa68e8fc2a1675a
unknown <nahomkeneni4@gmail.com>
2024-09-10 10:00:00 -0700

    new

--- management/main.py
+++ management/main.py
@@ -1,9 +1,9 @@
-# from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# from typing import List
-# from sqlalchemy import create_engine, Column, Integer, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # Database setup
 DATABASE_URL = "sqlite:///./employees.db"
